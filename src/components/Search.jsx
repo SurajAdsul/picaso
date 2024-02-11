d3a8bd4fa7e33eacd0904363abfe5e184aa574f3
Suraj Adsul <suraj.adsul@tech9.com>
2024-02-11 16:46:57 +0530

    Fix CSS

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -20,10 +20,10 @@ const Search = () => {
             </div>
             <input onChange={e => setValue(e.target.value)} type="search"
                    id="default-search"
-                   className="block items-center w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                   className="block items-center w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
                    placeholder="Search pictures..." required/>
             <button type="submit"
-                    className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search
+                    className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2">Search
             </button>
         </div>
     </form>);
